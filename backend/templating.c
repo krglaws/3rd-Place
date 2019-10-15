615d058eeda21b605cd26b2afcde5c82a72d8de1
Kyle Glaws <kyle.glaws@gmail.com>
2019-10-15 17:27:38 -0400

    working on templating

---
+++ backend/templating.c
@@ -0,0 +1,122 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+int check_braces(char* content)
+{
+	int len = strlen(content);
+	int depth = 0;
+	int pairs = 0;
+	
+	for (int i = 0; i < len; i++)
+	{
+		if (content[i] == '{') depth++;
+		else if (content[i] == '}') {depth--; pairs++;}
+		if (depth < 0 || depth > 1) return -1;
+	}
+	return pairs;
+}
+
+
+void fill_pair_list(char* contents, int* pair_list)
+{
+	int len = strlen(contents);
+	for (int i = 0; i < len; i++)
+		if (contents[i] == '{')
+		{
+			*pair_list = i;
+			pair_list++;
+		}
+		else if (contents[i] == '}')
+		{
+			*pair_list = i;
+			pair_list++;
+		}
+}
+
+
+char* insert_html(char* contents, char* replace, char* with)
+{
+	int num_pairs;
+	if ((num_pairs = check_braces(contents)) < 1) return NULL;
+	int pairs[2*num_pairs];
+	int result_size = strlen(contents) + (num_pairs * strlen(with));
+	char* result = malloc(result_size);
+	memset(result, 0, result_size);
+
+	/* identify all brace pairs */
+	fill_pair_list(contents, pairs);
+
+	/* replace occurances of 'replace' string */
+	int open, close, last_close = 0;
+	size_t bytes;
+	char* result_end = result;
+	char* contents_end = contents;
+
+	for (int i = 0; i < (num_pairs * 2); i+=2)
+	{
+		open = pairs[i];
+		close = pairs[i+1];
+		
+		bytes = (open - last_close);
+		memcpy(result_end, contents_end, bytes);
+		result_end += bytes;
+		contents_end += bytes;
+
+		if (strncmp(replace, contents_end, (close - open)+1) == 0)
+		{
+			memcpy(result_end, with, strlen(with));
+			result_end += strlen(with);
+		}
+		else 
+		{
+
+			memcpy(result_end, contents_end, (close - open) + 1);
+			result_end += (close - open) + 1;
+		}	
+		contents_end = &(contents[close+1]);
+		last_close = close + 1;
+	}
+
+	memcpy(result_end, contents_end, strlen(contents_end));
+
+	return result;
+}
+
+
+
+char* load_html(char* fname)
+{
+	FILE* fd;
+	int filelen;
+	char* contents;
+
+	fd = fopen(fname, "r");
+	if (fd == 0)
+	{
+		perror("Failed to open file");
+		return NULL;
+	}
+
+	fseek(fd, 0, SEEK_END);
+	filelen = ftell(fd);
+	contents = calloc(1, filelen);
+	rewind(fd);
+	
+	for (int i = 0; i < filelen; i++)
+		contents[i] = fgetc(fd);
+	
+	return contents;
+}
+
+
+int main()
+{
+	char* file = load_html("./test.html");
+	printf("File contents:\n%s\n", file);
+	printf(insert_html(file, "{TEST}", "<p> sup! <p>"));
+}
+
