ff071cfee4ec88f73c898ea3992a8fb40f83d2b1
Kyle Glaws <kyle.glaws@gmail.com>
2019-11-15 23:35:01 -0500

    update kylestructs usage

--- backend/conn_mgr.c
+++ backend/conn_mgr.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <kylestructs/datacont.h>
-#include <kylestructs/listnode.h>
+#include <kylestructs.h>
 
 #include "include/conn_mgr.h"
 
 
-static listnode* conns = NULL;
+static list conns = { NULL };
 static int num_conns = 0;
 
 
 void add_connection(int fd)
 {
-  if (conns == NULL)
-    conns = listnode_new(datacont_new(&fd, INT, 1));
-
-  else listnode_add(conns, datacont_new(&fd, INT, 1));
+  list_add(&conns, datacont_new(&fd, INT, 1));
 
   num_conns++;
 }
@@ -25,12 +21,11 @@ void remove_connection(int fd)
 {
   datacont* dc = datacont_new(&fd, INT, 1);
 
-  listnode_remove(conns, dc);
+  list_remove_by(&conns, dc);
 
   datacont_delete(dc);
 
-  if (--num_conns == 0)
-    conns = NULL;
+  --num_conns;
 }
 
 
@@ -43,7 +38,7 @@ int initialize_fdset(fd_set* fds)
 
   for (int i = 0; i < num_conns; i++)
   {
-    dc = listnode_get_nth(conns, i);
+    dc = list_get(&conns, i);
     FD_SET(dc->i, fds);
     if (max < dc->i) max = dc->i;
     datacont_delete(dc);
@@ -57,7 +52,7 @@ int get_active_fd(fd_set* fds)
   datacont* dc;
   for (int i = 0; i < num_conns; i++)
   {
-    dc = listnode_get_nth(conns, i);
+    dc = list_get(&conns, i);
     if (FD_ISSET(dc->i, fds))
     {
       int fd = dc->i;
