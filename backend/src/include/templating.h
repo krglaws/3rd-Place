5ceed78963d7b283010869778933dc644050d490
Kyle <krglaws@eckerd.edu>
2019-12-27 15:13:22 -0500

    templating.h: remove load_html() definition

--- backend/src/include/templating.h
+++ backend/src/include/templating.h
@@ -5,10 +5,8 @@
 int check_braces(char* content);
 
 void fill_pair_list(char* contents, int* pair_list);
-	
-char* insert_html(char* contents, char* replace, char* with);
 
-char* load_html(char* fname);
+char* insert_html(char* contents, char* replace, char* with);
 
 #endif
 
