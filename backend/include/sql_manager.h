5c2dcc79455ad91425e8e40349ac2db66c3ce3f1
Kyle Glaws <kyle.glaws@gmail.com>
2020-08-23 17:44:12 -0400

    fix up sql_manager.c

---
+++ backend/include/sql_manager.h
@@ -0,0 +1,10 @@
+
+#ifndef _SQL_MANAGER_H_
+#define _SQL_MANAGER_H_
+
+void init_sql_manager();
+
+char*** query_database(char* query);
+
+#endif
+
--- backend/src/sql_manager.c
+++ backend/src/sql_manager.c
@@ -2,65 +2,75 @@
 #include <stdio.h>
 #include <my_global.h>
 #include <mysql.h>
-#include <kylestructs.h>
 
 #include <sql_manager.h>
 
 
-MYSQL* sqlcon;
+static MYSQL* sqlcon;
 
 
 void init_sql_manager()
 {
   if ((sqlcon = mysql_init(NULL)) == NULL)
   {
-    fprintf(stderr, "%s", mysql_error(NULL));
+    fprintf(stderr, "%s\n", mysql_error(NULL));
     exit(EXIT_FAILURE);
   }
 
   if (mysql_real_connect(sqlcon, "localhost", "falcon", "falcon123",
 			  "falcondb", 3306, NULL, 0) == NULL)
   {
-    fprintf(stderr, "%s", mysql_error(NULL));
+    fprintf(stderr, "%s\n", mysql_error(NULL));
     mysql_close(sqlcon);
     exit(EXIT_FAILURE);
   }
 }
 
 
-list** query_database(char* query)
+char*** query_database(char* query)
 {
   MYSQL_RES* result;
   MYSQL_ROW sqlrow;
-  list** rows;
 
-  if (query == NULL) return NULL;
+  if (query == NULL)
+  {
+    fprintf(stderr, "query_database(): null query\n");
+    return NULL;
+  }
 
   if (mysql_query(sqlcon, query))
   {
-    fprintf(stderr, mysql_error(sqlcon));
+    fprintf(stderr, "%s\n", mysql_error(sqlcon));
     return NULL;
   }
 
   result = mysql_store_result(sqlcon);
   if (result == NULL)
   {
-    fprintf(stderr, mysql_error(sqlcon));
+    fprintf(stderr, "%s\n", mysql_error(sqlcon));
     return NULL;
   }
 
   int num_rows = mysql_num_rows(result);
-  rows = calloc(sizeof(list*) * (num_rows + 1));
+  char*** rows = calloc(num_rows + 1, sizeof(char**));
 
   for (int i = 0; i < num_rows; i++)
   {
-    rows[i] = list_new();
     sqlrow = mysql_fetch_row(result);
     int num_fields = mysql_num_fields(result);
+    rows[i] = calloc(num_fields + 1, sizeof(char*));
     for (int j = 0; j < num_fields; j++)
     {
-      if (sqlrow[j]) list_add(rows[i], datacont_new(sqlrow[j], CHARP, strlen(sqlrow[j])));
-      else list_add(rows[i], datacont_new("NULL", CHARP, 4));
+      if (sqlrow[j])
+      {
+        int fieldlen = strlen(sqlrow[j]);
+        rows[i][j] = malloc((fieldlen + 1) * sizeof(char));
+        memcpy(rows[i][j], sqlrow[j], fieldlen);
+      }
+      else
+      {
+        rows[i][j] = NULL;
+      }
     }
   }
 
