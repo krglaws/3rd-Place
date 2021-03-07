f110989a7a44179225ef6e05003ed9a6f5288d63
Kyle Glaws <kyle.glaws@gmail.com>
2021-03-07 18:01:10 -0500

    refactor validator.c

--- backend/include/validator.h
+++ backend/include/validator.h
@@ -1,10 +1,30 @@
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
 
-int init_validator();
+/* If any of the functions in this header return false,
+ * it means that the string input contained an invalid
+ * character or character encoding, did not meet the character
+ * requirement (only the case for passwords), or was either too
+ * long or too short.
+ */
 
-bool valid_name(const char* name, int max_len);
+/* These functions just check for valid characters,
+ * character requirements, and lengths.
+ */
+bool valid_user_name(const char* user_name);
+bool valid_community_name(const char* community_name);
 
-bool valid_content(char* dest, const char* src, int max_len);
+
+/* The function that calls these functions will need
+ * to allocate a separate buffer since the source string
+ * will be decoded from URL encoding and then re-encoded into
+ * HTML escaped strings (& -> &amp; for example).
+ */
+bool valid_passwd(char* dest, const char* src);
+bool valid_user_about(char* dest, const char* src);
+bool valid_community_about(char* dest, const char* src);
+bool valid_comment_body(char* dest, const char* src);
+bool valid_post_body(char* dest, const char* src);
+bool valid_post_title(char* dest, const char* src);
 
 #endif
--- backend/src/validator.c
+++ backend/src/validator.c
@@ -9,66 +9,86 @@
 #include <sql_manager.h>
 
 
-static ks_hashmap* url_code_map = NULL;
-static ks_hashmap* html_code_map = NULL;
+// used for converting percent encoded chars to ASCII
+static short get_hex_char_val(char c);
+static char hex_to_char(const char* hex);
+static int url_decode(char* dest, const char* src);
+
+static bool valid_content(char* dest, const char* src, int min_len, int max_len);
+static bool valid_name(const char* name, int min_len, int max_len);
+
+// encodes content into HTML-friendly form
+static void html_encode(char* dest, char* src);
 
 
-int init_validator()
+static short get_hex_char_val(char c)
 {
-  const char* url_code_path = "backend/url_encoding.txt";
-  char* url_code_file;
-  if ((url_code_file = load_file(url_code_path)) == NULL)
+  if (!isxdigit(c))
   {
-    log_crit("init_validator(): failed to open file '%s'", url_code_path);
+    return -1;
   }
 
-  url_code_map = string_to_map(url_code_file, "\n", ": ");
-  free(url_code_file);
-
-  const char* html_code_path = "backend/html_encoding.txt";
-  char* html_code_file;
-  if ((html_code_file = load_file(html_code_path)) == NULL)
+  if (isdigit(c))
   {
-    ks_hashmap_delete(url_code_map);
-    log_crit("init_validator(): failed to open file '%s'", html_code_path);
+    return c - '0';
   }
 
-  html_code_map = string_to_map(html_code_file, "\n", ": ");
-  free(html_code_file);
+  if (isupper(c))
+  {
+    return 10 + (c - 'A');
+  }
+
+  if (islower(c))
+  {
+    return 10 + (c - 'a');
+  }
+
+  return -1;
+}
+
+
+static char hex_to_char(const char* hex)
+{
+  short c1;
+  if ((c1 = get_hex_char_val(*hex)) == -1)
+  {
+    return '\0';
+  }
+
+  short c2;
+  if ((c2 = get_hex_char_val(*(hex+1))) == -1)
+  {
+    return '\0';
+  }
+
+  return (((char) c1) << 4) + ((char)c2);
 }
 
 
 static int url_decode(char* dest, const char* src)
 {
-  const char* c;
-  char key[4];
-
-  while (src[0] != '\0')
+  char c;
+  int i = 0, j = 0;
+  while (src[i] != '\0')
   {
-    if (src[0] == '%' && src[1] != '\0' && src[2] == '\0')
+    if (src[i] == '%' && src[i + 1] != '\0' && src[i + 2] != '\0')
     {
-      memcpy(key, src, 3);
-      if ((c = get_map_value_str(url_code_map, key)) == NULL)
+      if ((c = hex_to_char(src+i+1)) == '\0')
       {
         // invalid encoding
         return -1;
       }
-
-      *dest = *c;
-      dest++;
-      src += 3;
+      dest[j++] = c; 
+      i += 3;
     }
-    else if (src[0] == '+')
+    else if (src[i] == '+')
     {
-      *dest = ' ';
-      dest++;
-      src++;
+      dest[j++] = ' ';
+      i++;
     }
-    else if(isalnum(src[0]) || src[0] == '*' || src[0] == '-' || src[0] == '_')
+    else if(isalnum(src[i]) || src[i] == '*' || src[i] == '-' || src[i] == '_')
     {
-      *dest = *src;
-      dest++;
-      src++;
+      dest[j++] = src[i++];
     }
     else
     {
@@ -77,17 +97,17 @@ static int url_decode(char* dest, const char* src)
     }
   }
 
-  *dest = '\0';
+  dest[j] = '\0';
 
-  return 0;
+  return j;
 }
 
 
 static void html_encode(char* dest, char* src)
 {
-  while (src[0] != '\0')
+  while (*src != '\0')
   {
-    switch (src[0])
+    switch (*src)
     {
       case '&':
         memcpy(dest, "&amp;", 5);
@@ -112,40 +132,133 @@ static void html_encode(char* dest, char* src)
 }
 
 
-bool valid_name(const char* name, int max_len)
+bool valid_passwd(char* dest, const char* src)
 {
-  int len = strlen(name);
-
-  if (!(len < max_len))
+  // caller must allocate buffer for
+  // decoded password string
+  int len;
+  if ((len = url_decode(dest, src)) == -1)
   {
     return false;
   }
 
+  if (len < MIN_PASSWD_LEN)
+  {
+    return false;
+  }
+
+  // at least 1 upper, 1 lower, 1 digit
+  char c;
+  int upper = 0, lower = 0, number = 0;
   for (int i = 0; i < len; i++)
   {
-    char c = name[i];
+    c = dest[i];
+
+    if (len > MAX_PASSWD_LEN)
+    {
+      return false;
+    }
+
+    if (isupper(c))
+    {
+      upper++;
+    }
+
+    if (islower(c))
+    {
+      lower++;
+    }
+
+    if (isdigit(c))
+    {
+      number++;
+    }
+  }
+
+  return upper && lower && number;
+}
+
+
+static bool valid_name(const char* name, int min_len, int max_len)
+{
+  // no need to decode since valid name strings
+  // do not contain chars that would be encoded
+
+  char c;
+  int len = 0;
+  while ((c = name[len++]) != '\0')
+  {
+    if (len > max_len)
+    {
+      return false;
+    }
+
     if (!(isalnum(c) || c == '_'))
     {
       return false;
     }
   }
 
-  return true;
+  return len >= min_len;
 }
 
 
-bool valid_content(char* dest, const char* src, int max_len)
+bool valid_user_name(const char* user_name)
 {
-  char tmp[max_len];
-  if (url_decode(tmp, src))
+  return valid_name(user_name, MIN_USER_NAME_LEN, MAX_USER_NAME_LEN);
+}
+
+
+bool valid_community_name(const char* community_name)
+{
+  return valid_name(community_name, MIN_COMMUNITY_NAME_LEN, MAX_COMMUNITY_NAME_LEN);
+}
+
+
+static bool valid_content(char* dest, const char* src, int min_len, int max_len)
+{
+  // decoded string will always be <= src length
+  int len;
+  char tmp[max_len+1];
+  if ((len = url_decode(tmp, src)) == -1)
   {
     return false;
   }
 
-  if (!(strlen(tmp) < max_len))
+  if (len > max_len || len < min_len)
   {
     return false;
   }
 
   html_encode(dest, tmp);
+}
+
+
+bool valid_user_about(char* dest, const char* src)
+{
+  return valid_content(dest, src, MIN_USER_ABOUT_LEN, MAX_USER_ABOUT_LEN);
+}
+
+
+bool valid_community_about(char* dest, const char* src)
+{
+  return valid_content(dest, src, MIN_COMMUNITY_ABOUT_LEN, MAX_COMMUNITY_ABOUT_LEN);
+}
+
+
+bool valid_comment_body(char* dest, const char* src)
+{
+  return valid_content(dest, src, MIN_COMMENT_BODY_LEN, MAX_COMMENT_BODY_LEN);
+}
+
+
+bool valid_post_body(char* dest, const char* src)
+{
+  return valid_content(dest, src, MIN_POST_BODY_LEN, MAX_POST_BODY_LEN);
+}
+
+
+bool valid_post_title(char* dest, const char* src)
+{
+  return valid_content(dest, src, MIN_POST_TITLE_LEN, MAX_POST_TITLE_LEN);
 }
