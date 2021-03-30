17c7e724c963e9a6311547c1678778a419bb75b5
Kyle Glaws <kyle.glaws@gmail.com>
2021-03-29 21:42:39 -0400

    consolidate .css and .js files into 3p.css and 3p.js respectively. this has caused all kinds of issues with the styling due to overlapping class names, so need to fix all that. later will work on finishing the addition of options buttons throughout the site

--- 3p.js
+++ 3p.js
@@ -1,3 +1,93 @@
+function openUserTab(pageName, button) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("user-tab-content-container");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("user-tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "#CACBCC";
+  }
+
+  document.getElementById(pageName).style.display = "block";
+  button.style.backgroundColor = "white";
+}
+
+
+function openCommunityTab(pageName, button) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("community-tab-content-container");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("community-tab-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "#CACBCC";
+  }
+
+  document.getElementById(pageName).style.display = "block";
+  button.style.backgroundColor = "white";
+}
+
+
+function selectOverview() {
+  var tab = document.getElementById("default-tab");
+  if (tab !== null) {
+    tab.click();
+  }
+}
+
+
+function logOut() {
+
+  fetch("/logout", {
+    method: "POST",
+    redirect: "follow",
+    credentials: "same-origin"
+  }).then(response => {
+    if (response.redirected) {
+      window.location.href = response.url;
+    }
+    else {
+      console.log(response);
+      alert("Error");
+    }
+  });
+}
+
+
+function deleteUser(user_name) {
+
+  if (confirm("Are you sure you want to delete your account?") == false) {
+    return;
+  }
+
+  fetch("/delete_user?user_name="+user_name, {
+    method: "DELETE",
+    redirect: "follow",
+    credentials: "same-origin",
+  }).then(response => {
+    if (response.redirected) {
+      window.location.href = response.url;
+    }
+    else if (response.status == 403) {
+      alert("Permission denied");
+    }
+    else if (response.status == 404) {
+      alert("User does not exist");
+    }
+    else {
+      console.log(response);
+      alert("Error");
+    }
+  });
+}
+
+
 function findSibling(arrow, upOrDown) {
 
   var voteWrapper = arrow.parentElement.parentElement;
