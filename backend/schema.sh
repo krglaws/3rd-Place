559c040f78bd3b0673a4d21ee8531cb172af5e3d
Kyle Glaws <kyle.glaws@gmail.com>
2021-04-22 15:20:21 -0400

    increase post title length to 128 characters

--- backend/schema.sh
+++ backend/schema.sh
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS $DBNAME.posts(
   community_name VARCHAR(32) NOT NULL,
   author_id INT NOT NULL,
   author_name VARCHAR(16),
-  title VARCHAR(160) NOT NULL,
+  title VARCHAR(640) NOT NULL,
   body VARCHAR(2560),
   points INT DEFAULT 0,
   date_posted INT NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS $DBNAME.posts(
 CREATE TABLE IF NOT EXISTS $DBNAME.comments(
   id INT NOT NULL AUTO_INCREMENT,
   post_id INT NOT NULL,
-  post_title VARCHAR(160) NOT NULL,
+  post_title VARCHAR(640) NOT NULL,
   community_id INT NOT NULL,
   community_name VARCHAR(32) NOT NULL,
   author_id INT NOT NULL,
